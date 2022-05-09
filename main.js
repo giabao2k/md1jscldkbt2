5bbf5c65528413ca749de2442f44e1a5979bc4af
giabao2k <ngonguyengiabao2000@gmail.com>
2022-05-09 08:37:58 +0700

    bt luyentapifelse

---
+++ main.js
@@ -0,0 +1,7 @@
+let nhapchuoi = prompt("What is the “official” name of JavaScript?");
+if(nhapchuoi == 'ECMAScript'){
+    document.write('Right!')
+}
+else{
+    document.write('Didnt know? ECMAScript!')
+}
